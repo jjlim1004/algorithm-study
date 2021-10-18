a0bcd4b721c9b84eb77dc014da6ea93a6f741c95
imjeongjun <jjlim1004@naver.com>
2021-10-19 00:00:24 +0900

    프로그래머스 java 코딩테스트 레벨2 - 기능 개발 -stack 으로 구현하기

--- src/algorithm/programmers/level2/기능개발.java
+++ src/algorithm/programmers/level2/기능개발.java
@@ -3,44 +3,31 @@ package algorithm.programmers.level2;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
-class NewFunction{
-    int doneDate;
-
-    public NewFunction(int progress, int speed) {
-        doneDate = (int) Math.ceil((100-progress) / speed);
-    }
-
-}
-//93 30 55 / 1 30 5 == 7 3 9
 public class 기능개발 {
 
-    Queue<NewFunction> functionQ = new LinkedList<>();
-    int[] answer = {};
-
-    public void answerNumber(int queueSize){
-        ArrayList<Integer> functionList = new ArrayList<>();
-        for(int i = 0, j = 0 ,count =0; i< queueSize;i++ ){
-            if( functionQ.poll().doneDate >= functionQ.peek().doneDate){
-                functionList.add(j,count++);
-            }else {
-                count = 0;
-                functionList.add(j++,count++);
-            }
-        }
-        answer = new int[functionList.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = functionList.get(i);
-        }
-    }
-
     public int[] solution(int[] progresses, int[] speeds) {
+        Stack<Integer> stack = new Stack<Integer>();
+        ArrayList<Integer> s = new ArrayList<>();
 
-        for(int i =0 ; i< progresses.length; i++){
-            functionQ.add(new NewFunction(progresses[i], speeds[i]));
+        for (int i = progresses.length-1; i >= 0 ; i--) {
+            stack.add((int) Math.ceil((100-progresses[i]) / (double)speeds[i]));
         }
-        answerNumber(functionQ.size());
+
+        while(!stack.isEmpty()){
+            int cnt = 1;
+
+            int top =stack.pop();
+
+            while(!stack.isEmpty() && stack.peek() < top){
+                cnt++;
+                stack.pop();
+            }
+            s.add(cnt);
+        }
+
+        int[] answer = new int[s.size()];
         return answer;
     }
-
 }
