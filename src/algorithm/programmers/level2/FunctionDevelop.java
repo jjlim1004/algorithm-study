52b047f96238ed546bc90962aa8c84bdb073b89d
imjeongjun <jjlim1004@naver.com>
2022-03-06 23:59:03 +0900

    프로그래머스 lv2 스택/ 큐 기능개발(미완성)

--- src/algorithm/programmers/level2/FunctionDevelop.java
+++ src/algorithm/programmers/level2/FunctionDevelop.java
@@ -4,38 +4,33 @@ import java.util.*;
 
 public class FunctionDevelop {
 
-    // 구해야 되는것은 작업완료가 된 작업의 수, 그것들의 배열
-    // 
-    //
-    //
-    //
-    //
+    // 구해야 되는것은 작업완료가 된 작업의 수의 배열
 
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> needDatelist = new ArrayList<>();
         List<Integer> answer = new ArrayList<>();
         Queue<Integer> progressQ = new LinkedList<>();
-        int proceeding =0;
+        int proceedingDate =0;
 
         for (int progress: progresses) {
             progressQ.offer(progress);
         }
 
         for (int i = 0; i < speeds.length; i++) {
-            int num = progresses[i];
+            int num = 100-progresses[i];
             num = (int)Math.ceil(num/speeds[i]);
-            list.add(num);
+            needDatelist.add(num);
         }
 
-        progressQ.poll();
-
-        for (int i = 0; i < progresses.length-1; i++) {
-            if(proceeding < ){
-                proceeding++;
+        // 진행날짜가 같으면 poll 로 빼야된다.
+        for (int i = 0, j = 0; ;) {
+            if(proceedingDate == needDatelist.get(i)){
+                progressQ.poll();
+                j++;
+                i++;
             }
+
         }
-
-
         return answer;
     }
 }
