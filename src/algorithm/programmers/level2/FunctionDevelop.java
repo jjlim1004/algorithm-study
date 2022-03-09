567c102bde9535c6db5c27bcd475f753163c77b2
imjeongjun <jjlim1004@naver.com>
2022-03-10 00:58:27 +0900

    프로그래머스 lv2 스택/ 큐 기능개발 완성 & 통과

--- src/algorithm/programmers/level2/FunctionDevelop.java
+++ src/algorithm/programmers/level2/FunctionDevelop.java
@@ -7,30 +7,31 @@ public class FunctionDevelop {
     // 구해야 되는것은 작업완료가 된 작업의 수의 배열
 
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> needDatelist = new ArrayList<>();
+        //답을 위한 list
         List<Integer> answer = new ArrayList<>();
-        Queue<Integer> progressQ = new LinkedList<>();
-        int proceedingDate =0;
-
-        for (int progress: progresses) {
-            progressQ.offer(progress);
-        }
+        Queue<Integer> q = new LinkedList<>();
 
         for (int i = 0; i < speeds.length; i++) {
-            int num = 100-progresses[i];
-            num = (int)Math.ceil(num/speeds[i]);
-            needDatelist.add(num);
-        }
+            double num = 100-progresses[i];
+            int date = (int)Math.ceil(num/(double)speeds[i]);
 
-        // 진행날짜가 같으면 poll 로 빼야된다.
-        for (int i = 0, j = 0; ;) {
-            if(proceedingDate == needDatelist.get(i)){
-                progressQ.poll();
-                j++;
-                i++;
+            if(!q.isEmpty() && q.peek() < date ){
+
+                answer.add(q.size());
+                q.clear();
             }
+            q.offer(date);
 
         }
-        return answer;
+
+        answer.add(q.size());
+
+        //답의 array
+        int[] answerArray = new int[answer.size()];
+        for (int i = 0; i < answerArray.length; i++) {
+            answerArray[i] = answer.get(i);
+        }
+
+        return answerArray;
     }
 }
