d51fb8a75bbd9b57e86d3f45dc47b258b3b1aedd
jjim1004 <jjlim1004@naver.com>
2025-04-07 23:11:41 +0900

    feat: leetcode test commit
    -25.04.06
    easy:
    2220. Minimum Bit Flips to Convert Number

---
+++ src/algorithm/leetcode/easy/MinimumBitFlipsToConvertNumber.java
@@ -0,0 +1,13 @@
+package algorithm.leetcode.easy;
+
+public class MinimumBitFlipsToConvertNumber {
+    public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+        int ans = 0;
+        while(xor > 0 ){
+            ans += xor & 1;
+            xor >>= 1;
+        }
+        return ans;
+    }
+}
